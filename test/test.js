45fb3e59a77f079e2a29898aa63946934fb95772
Liran Tal <liran.tal@gmail.com>
2015-11-09 21:22:06 +0200

    adding support for tests with should and mocha, and added one test to the suite

---
+++ test/test.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var should = require('should');
+var AGM = require('../index.js');
+
+describe('agilemanager-api test suite', function () {
+
+  var options = {
+    clientId: '',
+    clientSecret: ''
+  };
+
+  var agm = new AGM(options);
+
+  describe('method login', function() {
+
+    it('should return an auth token', function(done) {
+
+      agm.login(function (err, body){
+
+        should.not.exist(err);
+
+        body.should.be.an.instanceof(Object).and.have.property('access_token');
+        body.should.be.an.instanceof(Object).and.have.property('token_type');
+        body.should.be.an.instanceof(Object).and.have.property('expires_in');
+        body.should.be.an.instanceof(Object).and.have.property('scope');
+
+        done();
+
+      });
+    });
+
+  });
+
+});
